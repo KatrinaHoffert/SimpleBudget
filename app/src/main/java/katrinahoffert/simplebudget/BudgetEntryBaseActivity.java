a1a8d48106d41de30f02b55a20f53574a0c920f7
Kat Hoffert <kat.hoffert@gmail.com>
2017-02-14 19:36:49 -0600

    Date picker working

--- app/src/main/java/katrinahoffert/simplebudget/BudgetEntryBaseActivity.java
+++ app/src/main/java/katrinahoffert/simplebudget/BudgetEntryBaseActivity.java
@@ -1,5 +1,6 @@
 package katrinahoffert.simplebudget;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,10 +10,13 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -41,7 +45,6 @@ public abstract class BudgetEntryBaseActivity extends AppCompatActivity {
         // that should be selected.
         categories =  CategoryDbManager.getCategories(this);
         String[] categoryNames = new String[categories.size()];
-        int defaultSelected = 0;
         for(int i = 0; i < categories.size(); ++i) {
             categoryNames[i] = categories.get(i).category;
         }
@@ -64,13 +67,15 @@ public abstract class BudgetEntryBaseActivity extends AppCompatActivity {
         dateInput.setText(date);
 
         amountInput = (EditText) findViewById(R.id.amountInput);
+
+        initializeDatePicker();
     }
 
     /**
      * Parses user input from the fields into a BudgetEntry object.
      * @return A BudgetEntry object that is valid in all but its "_id" field.
      */
-    public BudgetEntry parseInput() {
+    protected BudgetEntry parseInput() {
         int categoryId = categories.get(categorySelect.getSelectedItemPosition())._id;
         String amount = amountInput.getText().toString();
 
@@ -97,14 +102,37 @@ public abstract class BudgetEntryBaseActivity extends AppCompatActivity {
         if(centsAmount != 0 && amountSplit[1].length() == 1) centsAmount *= 10;
         int amountInCents = sign * (Math.abs(dollarAmount) + centsAmount);
 
-        // TODO: Validate date
-
         BudgetEntry entry = new BudgetEntry();
         entry.amount = amountInCents;
         entry.category = categorySelect.getSelectedItem().toString();
         entry.categoryId = categoryId;
         entry.date = dateInput.getText().toString();
         return entry;
+    }
+
+    /** Initializes the date picker to show when the dateInput is selected. */
+    private void initializeDatePicker() {
+        final Calendar calendar = Calendar.getInstance();
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, day);
+                dateInput.setText(new SimpleDateFormat("yyyy-MM-dd").format(calendar.getTime()));
+            }
+        };
+
+        dateInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Initialize date picker to the currently selected date and show it
+                String selectedDate = dateInput.getText().toString();
+                String[] splitDate = selectedDate.split("-");
+                new DatePickerDialog(BudgetEntryBaseActivity.this, date, Integer.parseInt(splitDate[0]),
+                        Integer.parseInt(splitDate[1]) - 1, Integer.parseInt(splitDate[2])).show();
+            }
+        });
     }
 
     protected abstract void submitButtonPressed();
